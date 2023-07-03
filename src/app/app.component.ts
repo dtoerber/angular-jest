ed410aa523e9c8bba85b74578885c623b578a0e2
Dave Toerber <dave@Daves-MacBook-Pro.local>
2023-07-03 12:02:43 -0600

    Initial configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<app-first></app-first>`,
 })
 export class AppComponent {
   title = 'jest';
